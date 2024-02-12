5a75a6698d44a7a7bc751e1d673e71aa4ab0574d
mazik7512 <mazik7512@yandex.ru>
2024-02-12 12:55:53 +0300

    small reformat

--- OpenLowLevelTerminal/OpenLowLevelTerminal.h
+++ OpenLowLevelTerminal/OpenLowLevelTerminal.h
@@ -10,6 +10,7 @@
 
 namespace OLLT {
 	
+	template<Tools::Platforms platform>
 	class LLTerminal final {
 
 	public:
@@ -17,31 +18,19 @@ namespace OLLT {
 		void write(std::string_view data);
 		std::string_view read();
 
-		template <Tools::Platforms platform, class ... Args>
+		template <class ... Args>
 		void format(const Tools::Coord_<platform> &coords, const unsigned int length, Args ... args);
 
 
 		void set_title(const std::string_view title);
-
-		template <Tools::Platforms platform>
 		void set_window_size(const Tools::Size_<platform> &size);
-
-		template <Tools::Platforms platform>
 		void set_codepage(const Tools::CodePage_<platform>& cp);
-
-		template <Tools::Platforms platform>
 		void set_font(const Tools::Font_<platform> &font);
 		
 
 		std::string_view get_title();
-
-		template <Tools::Platforms platform>
 		const Tools::Size_<platform>* get_windows_size();
-
-		template <Tools::Platforms platform>
 		const Tools::CodePage_<platform>* get_codepage();
-
-		template<Tools::Platforms platform>
 		const Tools::Font_<platform>* get_font();
 
 	};
